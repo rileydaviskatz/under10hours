9a7945000540d18e21279c2e1d3734cd8793872a
Riley Katz <riley.davis.katz@gmail.com>
2022-07-14 15:18:34 -0500

    bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TransparentTV from "./img/TransparentTV.png";
+import staticgif from "./img/static.gif";
+import './App.css';
+
+
 
 const App = () => {
-    return(
-            <h1>HELLO WORLD</h1>
-    );
+    return (
+        <div className="imgContainer">
+            <img src={TransparentTV} height="100%" className="crt"></img>
+            <img src={staticgif}  className="staticgif"></img>
+            <h1 className='titleText'>Under10Hours</h1>
+            <a id="PatreonLink" className="links" href="https://www.patreon.com/under10hours">
+                <h1>Patreon</h1>
+            </a>
+        </div>
+      );
 };
 
 export default App;
